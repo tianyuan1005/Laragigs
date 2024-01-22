d318435b287e37d9aacc29ca9116ca5ff3ab0fc7
tian yuan <tianyuan@192.168.1.105>
2024-01-22 22:10:01 +0000

    show listings

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Models\Listing;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ListingController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// All listings
+Route::get('/', [ListingController::class, 'index']);
+
+// Singe Listing
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
